a4ff97e7bca0f962b7e790b04e01bdece339539c
raddevus <1radeutsch1@gmail.com>
2025-04-07 15:05:41 -0400

    continuing work to fill out classes - working on basics for MailService to get some code to retrieve email

---
+++ BlueBoxApi/Interfaces/IMailService.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace NewLibre;
+
+interface IMailService{
+  int GetMail(int numberOfMessages);
+} 
---
+++ BlueBoxApi/Services/MailService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NewLibre;
+
+class MailService: IMailService{
+ public int GetMail(int numberOfMessages){
+     return 0;
+   }
+}
