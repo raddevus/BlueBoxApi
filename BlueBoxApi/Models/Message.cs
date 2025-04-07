0ffdcf296f111addf53e6bbf9b693b00cb28c976
raddevus <1radeutsch1@gmail.com>
2025-04-07 14:59:38 -0400

    building up model classes to contain fields we need

--- BlueBoxApi/Models/Message.cs
+++ BlueBoxApi/Models/Message.cs
@@ -5,5 +5,5 @@ class Message{
 	public string From{get;set;}
 	public string Body{get;set;}
 	public string Subject{get;set;}
-
+   public string FileName{get;set;} // file where email is saved
 }
---
+++ BlueBoxApi/Models/ServerConfiguration.cs
@@ -0,0 +1,10 @@
+using System;
+namespace NewLibre;
+
+class ServerConfiguration{
+   string AccountName{get;set;} // for user reminder
+   string ServerUri{get;set;}
+   string EmailAddress{get;set;}
+   string Password{get;set;} // encryption to be done later
+   string LocalStorePath{get;set;} // where all email for this account will be saved locally
+}
