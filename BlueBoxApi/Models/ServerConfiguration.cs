c2124f1a0c0237ae9aee857249088fc28f3741cb
raddevus <1radeutsch1@gmail.com>
2025-04-09 15:26:44 -0400

    ServerConfig with basic validation.  Code that uses ServerConfig should check that ValidationErrors.Count = 0 (!= > 0) before using the config

--- BlueBoxApi/Models/ServerConfiguration.cs
+++ BlueBoxApi/Models/ServerConfiguration.cs
@@ -8,4 +8,26 @@ class ServerConfiguration{
    public string Password{get;set;} // encryption to be done later
    public string LocalStorePath{get;set;} // where all email for this account will be saved locally
    public int Port{get;set;} = 993;
+   // Validate will run on construtor
+   // Then user checks if ValidationErrors.Count > 0
+   public List<string> ValidationErrors = new();
+   
+   public ServerConfiguration(string serverUri, string emailAddress, string password, string localStorePath){
+      ServerUri = serverUri;
+      EmailAddress = emailAddress;
+      Password = password;
+      LocalStorePath = localStorePath;
+      Validate();
+   }
+
+   private void Validate(){
+
+      if (!Directory.Exists(LocalStorePath)){
+         ValidationErrors.Add($"LocalStorePath {LocalStorePath} doesn't exist."); 
+      }
+
+      if (String.IsNullOrEmpty(ServerUri)){
+         ValidationErrors.Add($"ServerUri is blank. Please configure properly.");
+      }
+   }
 }
