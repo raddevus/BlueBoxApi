7607b1acfbabaea2ce38e11d92b5bbd4be277917
raddevus <1radeutsch1@gmail.com>
2025-04-09 14:10:28 -0400

    Continuing work to complete basic mail retrieval

--- BlueBoxApi/Models/ServerConfiguration.cs
+++ BlueBoxApi/Models/ServerConfiguration.cs
@@ -7,4 +7,5 @@ class ServerConfiguration{
    string EmailAddress{get;set;}
    string Password{get;set;} // encryption to be done later
    string LocalStorePath{get;set;} // where all email for this account will be saved locally
+   int Port{get;set;} = 993;
 }
--- BlueBoxApi/Services/MailService.cs
+++ BlueBoxApi/Services/MailService.cs
@@ -3,7 +3,13 @@ using System;
 namespace NewLibre;
 
 class MailService: IMailService{
- public int GetMail(int numberOfMessages){
-     return 0;
+   public ServerConfiguration Config{get;set;}
+   MailService(ServerConfiguration config){
+      this.Config = config;
+   }
+
+   public int GetMail(int numberOfMessages){
+      
+    return 0;
    }
 }
